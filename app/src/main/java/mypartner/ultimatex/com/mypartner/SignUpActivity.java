41208a519215b49182e7c33aaec6cf47bc57c95d
lakinduakash <lakinduakash@gmail.com>
2018-06-10 13:00:14 +0530

    add activities and layouts

---
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/SignUpActivity.java
@@ -0,0 +1,13 @@
+package mypartner.ultimatex.com.mypartner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
