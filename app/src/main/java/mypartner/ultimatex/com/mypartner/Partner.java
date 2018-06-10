b55d0b0d287d174391bc72b81aa191b246b0d53d
lakinduakash <lakinduakash@gmail.com>
2018-06-11 00:49:16 +0530

    create partner class

--- app/src/main/java/mypartner/ultimatex/com/mypartner/Partner.java
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/Partner.java
@@ -4,4 +4,100 @@ public class Partner {
 
     public static final String MALE="male";
     public static final String FEMALE="female";
+
+    private String email;
+    private String homeCity;
+    private int age;
+    private String gender;
+    private String cast;
+    private String religion;
+    private String other;
+    private String contact;
+    private String height;
+    private String password;
+
+    public Partner(String email) {
+        this.email = email;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomeCity() {
+        return homeCity;
+    }
+
+    public void setHomeCity(String homeCity) {
+        this.homeCity = homeCity;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
