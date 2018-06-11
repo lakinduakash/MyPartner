9b483a247a419204a651e155aa3ed4554adb672b
lakinduakash <lakinduakash@gmail.com>
2018-06-11 23:31:41 +0530

    create login function

--- app/src/main/java/mypartner/ultimatex/com/mypartner/util/Connection.java
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/util/Connection.java
@@ -1,4 +1,40 @@
 package mypartner.ultimatex.com.mypartner.util;
 
+import mypartner.ultimatex.com.mypartner.model.LoginRequest;
+import mypartner.ultimatex.com.mypartner.model.LoginResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 public class Connection {
+
+    private static Retrofit retrofit;
+    private static Connection connection;
+
+    private final String BASE_URL = "http://192.168.1.101:8000/";
+
+    private Connection() {
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static Connection getInstance() {
+        if (retrofit == null || connection == null)
+            return connection = new Connection();
+        else
+            return connection;
+    }
+
+    public void login(LoginRequest request, Callback<LoginResponse> responseCallback) {
+        PartnerService partnerService = retrofit.create(PartnerService.class);
+
+        Call<LoginResponse> loginResponseCall = partnerService.login(request);
+        loginResponseCall.enqueue(responseCallback);
+    }
+
 }
