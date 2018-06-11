bad19d358363236cf779872c44401145f8079124
lakinduakash <lakinduakash@gmail.com>
2018-06-12 02:13:48 +0530

    add login functionality

--- app/src/main/java/mypartner/ultimatex/com/mypartner/LoginActivity.java
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/LoginActivity.java
@@ -1,5 +1,6 @@
 package mypartner.ultimatex.com.mypartner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String LOGGED_IN_KEY = "my_key_logged_in";
+    public static final String LOGGED_IN_VALUE = "my_value_logged_in";
 
     Button loggingButton;
     Button signUpButton;
@@ -34,6 +38,14 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         errorText = findViewById(R.id.errorText);
 
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
         loggingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,8 +62,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 if (response.code() == 200) {
                     boolean a = response.body().isLoggedIn();
-                    if (a)
+                    if (a) {
                         errorText.setText(response.body().getEmail());
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.putExtra(LOGGED_IN_KEY, LOGGED_IN_VALUE);
+                        startActivity(intent);
+                        finish();
+                    }
                     else
                         errorText.setText("" + response.code());
                 } else {
