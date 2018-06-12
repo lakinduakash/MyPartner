7ef8e14ff521bf6408cbf86b67a01e20de68af65
lakinduakash <lakinduakash@gmail.com>
2018-06-12 19:25:14 +0530

    add Recycler Item Listener

---
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/RecyclerItemClickListener.java
@@ -0,0 +1,51 @@
+package mypartner.ultimatex.com.mypartner;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+
+    private OnClickListener mListener;
+    private GestureDetector mGestureDetector;
+
+    public RecyclerItemClickListener(Context context, OnClickListener listener) {
+        mListener = listener;
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        View childView = rv.findChildViewUnder(e.getX(), e.getY());
+        if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e) && rv.getScrollState() == RecyclerView.SCROLL_STATE_IDLE) {
+            mListener.onClick(childView, rv.getChildAdapterPosition(childView));
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+
+    public interface OnClickListener {
+        void onClick(View view, int position);
+    }
+}
+
