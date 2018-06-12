79add1bce339cf07fc5a27216660a1d148df43d2
lakinduakash <lakinduakash@gmail.com>
2018-06-12 19:27:09 +0530

    add signup response model class

---
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/model/SignUpResponse.java
@@ -0,0 +1,39 @@
+package mypartner.ultimatex.com.mypartner.model;
+
+public class SignUpResponse {
+
+    private String email;
+    private boolean loggedIn;
+    private boolean success;
+
+    public SignUpResponse(String email, boolean loggedIn, boolean success) {
+        this.email = email;
+        this.loggedIn = loggedIn;
+        this.success = success;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+}
