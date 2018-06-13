f94468b4ffa593afd94851f853b7d0bbb25d56df
lakinduakash <lakinduakash@gmail.com>
2018-06-13 13:47:43 +0530

    change email to username

--- app/src/main/java/mypartner/ultimatex/com/mypartner/model/LoginRequest.java
+++ app/src/main/java/mypartner/ultimatex/com/mypartner/model/LoginRequest.java
@@ -1,20 +1,20 @@
 package mypartner.ultimatex.com.mypartner.model;
 
 public class LoginRequest {
-    private String email;
+    private String username;
     private String password;
 
-    public LoginRequest(String email, String password) {
-        this.email = email;
+    public LoginRequest(String username, String password) {
+        this.username = username;
         this.password = password;
     }
 
     public String getEmail() {
-        return email;
+        return username;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
